62d6c1d35e6786e76ce59604bb466ebf4c85cc8a
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2023-07-29 13:10:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const Papa = require('papaparse');
+const bodyParser = require('body-parser');
 
 
-
+// Set the maximum payload size (e.g., 10mb)
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 
 app.use(cors());
@@ -25,7 +28,7 @@ app.post('/', async (req, res) => {
     const csvData = Papa.unparse(data, { newline: '\n' });
     fs.writeFileSync('./public/data.csv', csvData);
 
-    res.status(200).json(data)
+    res.status(200).json(data);
 
 
 })
